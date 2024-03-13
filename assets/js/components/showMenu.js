e88ae36bc20399fc6f25a8c0a4db70c72f8b0d05
Hiroshi Watanabe <archistudioh2@gmail.com>
2024-03-12 20:25:38 -0500

    Diseño para celulares al 75%

--- assets/js/components/showMenu.js
+++ assets/js/components/showMenu.js
@@ -1,13 +1,16 @@
 function showMenu() {
-    const nav = document.querySelector('.logo');
+    const nav = document.querySelector('.nav');
     const menu = document.querySelector('.nav__menu')
 
     nav.addEventListener('click', function (event) {
         if (event.target.closest('.btn__menu')) {
             menu.classList.toggle('show--menu')
         }
-    })
 
+        if (event.target.closest('.nav__link')) {
+            menu.classList.remove('show--menu')
+        }
+    })
 }
 
 export default showMenu
